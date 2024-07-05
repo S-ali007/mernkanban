8995e36cbcf064faca2040a2799cdae46384ec98
ali-007 <ali.techtribe007@gmail.com>
2024-07-05 20:19:46 +0500

    first

---
+++ backend/db/connect.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import User from "./models/userSchema.js";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // Increase the timeout as needed
+      socketTimeoutMS: 45000, // Increase the socket timeout as needed
+    });
+    console.log(conn.connection.host);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function run() {
+  const user = await User.find();
+  // console.log(user)
+}
+
+run();
+
+export default connectDB;
